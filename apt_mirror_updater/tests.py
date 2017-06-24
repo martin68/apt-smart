89626dae5937fe01237861cdb195d977b138ce43
Peter Odding <peter@peterodding.com>
2017-06-24 08:24:47 +0200

    Move test helpers to humanfriendly.testing

--- apt_mirror_updater/tests.py
+++ apt_mirror_updater/tests.py
@@ -1,7 +1,7 @@
 # Automated, robust apt-get mirror selection for Debian and Ubuntu.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: June 14, 2017
+# Last Change: June 24, 2017
 # URL: https://apt-mirror-updater.readthedocs.io
 
 """Test suite for the ``apt-mirror-updater`` package."""
@@ -9,14 +9,10 @@
 # Standard library modules.
 import logging
 import os
-import sys
-import unittest
 
 # External dependencies.
-import coloredlogs
 from executor import execute
-from humanfriendly import compact
-from six.moves import StringIO
+from humanfriendly.testing import TestCase, run_cli
 
 # Modules included in our package.
 from apt_mirror_updater import AptMirrorUpdater
@@ -26,29 +22,9 @@ from apt_mirror_updater.cli import main
 logger = logging.getLogger(__name__)
 
 
-class AptMirrorUpdaterTestCase(unittest.TestCase):
+class AptMirrorUpdaterTestCase(TestCase):
 
     """:mod:`unittest` compatible container for the :mod:`apt_mirror_updater` test suite."""
-
-    def setUp(self):
-        """Enable verbose logging and reset it after each test."""
-        coloredlogs.install(level='DEBUG')
-
-    def skipTest(self, text, *args, **kw):
-        """
-        Enable backwards compatible "marking of tests to skip".
-
-        By calling this method from a return statement in the test to be
-        skipped the test can be marked as skipped when possible, without
-        breaking the test suite when unittest.TestCase.skipTest() isn't
-        available.
-        """
-        reason = compact(text, *args, **kw)
-        try:
-            super(AptMirrorUpdaterTestCase, self).skipTest(reason)
-        except AttributeError:
-            # unittest.TestCase.skipTest() isn't available in Python 2.6.
-            logger.warning("%s", reason)
 
     def test_debian_mirror_discovery(self):
         """Test the discovery of Debian mirror URLs."""
@@ -83,7 +59,7 @@ class AptMirrorUpdaterTestCase(unittest.TestCase):
 
     def test_current_mirror_discovery(self):
         """Test that the current mirror can be extracted from ``/etc/apt/sources.list``."""
-        exit_code, output = run_cli('--find-current-mirror')
+        exit_code, output = run_cli(main, '--find-current-mirror')
         assert exit_code == 0
         assert is_mirror_url(output.strip())
 
@@ -153,24 +129,3 @@ def is_ubuntu_mirror(url):
 def has_compatible_scheme(url):
     """Check whether the given URL uses a scheme compatible with and intended to be used by apt."""
     return url.startswith(('http://', 'https://'))
-
-
-def run_cli(*arguments):
-    """Simple wrapper to run :func:`apt_mirror_updater.cli.main()` in the same process."""
-    saved_argv = sys.argv
-    saved_stderr = sys.stderr
-    saved_stdout = sys.stdout
-    fake_stdout = StringIO()
-    try:
-        sys.argv = ['apt-mirror-updater'] + list(arguments)
-        sys.stdout = fake_stdout
-        sys.stderr = fake_stdout
-        main()
-        exit_code = 0
-    except SystemExit as e:
-        exit_code = e.code
-    finally:
-        sys.argv = saved_argv
-        sys.stderr = saved_stderr
-        sys.stdout = saved_stdout
-    return exit_code, fake_stdout.getvalue()
