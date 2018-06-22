1097a70fddf06e4c9afff7a301bac03832597721
Peter Odding <peter@peterodding.com>
2018-06-22 12:32:44 +0200

    Enable test_gather_eol_dates on Travis CI

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -10,3 +10,14 @@ pip-accel install --requirement=requirements-tests.txt
 # Install the project itself, making sure that potential character encoding
 # and/or decoding errors in the setup script are caught as soon as possible.
 LC_ALL=C pip-accel install .
+
+# Let apt-get, dpkg and related tools know that we want the following
+# commands to be 100% automated (no interactive prompts).
+export DEBIAN_FRONTEND=noninteractive
+
+# Update apt-get's package lists.
+sudo -E apt-get update -qq
+
+# Make sure the /usr/share/distro-info/*.csv files are available,
+# this enables the test_gather_eol_dates() test.
+sudo -E apt-get install --yes distro-info-data
