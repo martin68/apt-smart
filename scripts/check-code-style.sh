53d15f08a1b8513b6dc4fc7ba4f1c477474a32bc
m <martin68@users.noreply.github.com>
2019-09-12 20:52:25 +0800

    Disable code style check for Python 2.7 because Python 2.7 also has an issue with flake8 now.

--- scripts/check-code-style.sh
+++ scripts/check-code-style.sh
@@ -4,11 +4,12 @@
 # value compatibility with Python 2.6, however recently it seems that flake8
 # has dropped Python 2.6 compatibility. That's only fair, but now I need to
 # work around it, hence this trivial script :-).
+# Python 2.7 also has an issue with flake8 now, so disable it for Python 2.7
 
-if python -c 'import sys; sys.exit(0 if sys.version_info[:2] >= (2, 7) else 1)'; then
+if python -c 'import sys; sys.exit(0 if sys.version_info[:2] > (2, 7) else 1)'; then
   echo "Updating installation of flake8 .." >&2
   pip-accel install --upgrade --quiet --requirement=requirements-checks.txt
   flake8
 else
-  echo "Skipping code style checks on Python 2.6 .." >&2
+  echo "Skipping code style checks on Python 2.7 .." >&2
 fi
