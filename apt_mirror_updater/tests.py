73959f3e726e571e8dfaca2b08e7ea5a324e62b0
m <martin68@users.noreply.github.com>
2019-09-12 16:08:53 +0800

    Add test case: `test_report_available_mirrors` to test that print the available mirrors to the terminal.

--- apt_mirror_updater/tests.py
+++ apt_mirror_updater/tests.py
@@ -77,6 +77,11 @@ class AptMirrorUpdaterTestCase(TestCase):
         assert exit_code == 0
         check_mirror_url(output.strip())
 
+    def test_report_available_mirrors(self):
+        """Test that print the available mirrors to the terminal."""
+        exit_code, output = run_cli(main, '--list-mirrors')
+        assert exit_code == 0
+
     def test_dumb_update(self):
         """Test that our dumb ``apt-get update`` wrapper works."""
         if os.getuid() != 0:
