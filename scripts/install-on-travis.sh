47c5ba276edd650d55d9ef2ec6afbae9ffcc4d61
martin68 <6174552+martin68@users.noreply.github.com>
2025-01-24 22:30:11 +0800

    try to fix github actions python 3.12 ModuleNotFoundError: No module named 'pkg_resources'

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -3,6 +3,9 @@
 # Install the required Python packages.
 pip install --requirement=requirements-travis.txt
 
+# For github actions python 3.12, install within virtualenv.
+/home/runner/.virtualenvs/apt-smart/bin/pip install --upgrade pip setuptools wheel
+
 # Install the project itself, making sure that potential character encoding
 # and/or decoding errors in the setup script are caught as soon as possible.
 LC_ALL=C pip install .
